541d57ae2272e03fbb97aad4e4a15e71e416ac91
Alejandra Vasquez <maria.vasquez66@correo.tdea.edu.co>
2026-08-29 19:34:20 -0500

    Administrar devoluciones y reembolsos

---
+++ src/main/java/application/domain/entities/Refund.java
@@ -0,0 +1,19 @@
+package application.domain.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Refund {
+    private String id;
+    private Return returnRequest;
+    private Double amount;
+    private String paymentMethod;
+
+    public Refund(Return returnRequest, Double amount, String paymentMethod) {
+        this.returnRequest = returnRequest;
+        this.amount = amount;
+        this.paymentMethod = paymentMethod;
+    }
+}
